6b44fa0068d3c6c275390d60516786058011d65f
Karma-234 <busari1kamal@gmail.com>
2023-10-02 11:58:43 -0700

    Added supabase

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const LoginPage = () => {
@@ -8,5 +8,12 @@ const LoginPage = () => {
     </View>
   )
 }
-
+const style = StyleSheet.create({
+  container: {flex: 1, 
+              flexDirection: 'column', 
+              backgroundColor: '#150E28', 
+              justifyContent: 'center',
+              alignItems:'center',
+              },
+});
 export default LoginPage;
